000f60f2b34bb901ffe2c1e6522d8c0e4c5ff80a
Micheal Leavitt <micheal.leavitt@popagency.com>
2013-02-13 10:56:53 -0800

    Decoupled Marionette.Callbacks depency on jQuery.Deffered
    Setting Marionette.compatMode based on check against $ version string.
    If major is less than 2 and minor is less than 5 Marionette.Callbacks
    will now return warning messages instead of failing. Obviously the user
    of this version will not be able to safely use Marionette.Callbacks so
    the check is really there to allow for forwards compatibility in a
    project that currently uses less than jQuery 1.5 but may at an
    indeterminate time in the future be upgraded to a more recent version.
    In short, this keeps it from breaking now and gives the developer/user
    a warning message indicating when $.Deffered isn't supported.

--- src/marionette.callbacks.js
+++ src/marionette.callbacks.js
@@ -1,10 +1,27 @@
 // Callbacks
 // ---------
 
+// NOTE: Marionette v1.0.0-rc5 is no longer compatible with jQuery < 1.5.0
+// To know if we need to shim the incompatibilities we test the version
+// @author micheall <micheal.leavitt@popagency.com>
+Marionette.compatMode = function() {
+  var $version = $().jquery.split('.');
+  return $version[0] < 2 && $version[1] < 5; 
+};
+
 // A simple way of managing a collection of callbacks
 // and executing them at a later point in time, using jQuery's
 // `Deferred` object.
 Marionette.Callbacks = function(){
+  // NOTE: modified for jQuery < 1.5.0 compatibility
+  // @author micheall <micheal.leavitt@popagency.com>
+  if(Marionette.compatMode) {
+    var msg = "$.Deffered not supported by jQuery " + $().jquery + ". Callbacks will not be executed.";
+    if(!_.isUndefined(window.console)) {
+      window.console.warn(msg);
+    }
+    return msg;
+  }
   this._deferred = $.Deferred();
   this._callbacks = [];
 };
@@ -15,6 +32,15 @@ _.extend(Marionette.Callbacks.prototype, {
   // guaranteed to execute, even if they are added after the 
   // `run` method is called.
   add: function(callback, contextOverride){
+    // NOTE: modified for jQuery < 1.5.0 compatibility
+    // @author micheall <micheal.leavitt@popagency.com>
+    if( Marionette.compatMode ) {
+      var msg = "$.Deffered not supported by jQuery " + $().jquery + ". Callback will not be added.";
+      if(!_.isUndefined(window.console)) {
+        window.console.warn(msg);
+      }
+      return msg;
+    } 
     this._callbacks.push({cb: callback, ctx: contextOverride});
 
     this._deferred.done(function(context, options){
@@ -27,12 +53,30 @@ _.extend(Marionette.Callbacks.prototype, {
   // Additional callbacks can be added after this has been run 
   // and they will still be executed.
   run: function(options, context){
+    // NOTE: modified for jQuery < 1.5.0 compatibility
+    // @author micheall <micheal.leavitt@popagency.com>
+    if(Marionette.compatMode) { 
+      var msg = "$.Deffered not supported by jQuery " + $().jquery + ". Callbacks will not run.";
+      if(!_.isUndefined(window.console)) {
+        window.console.warn(msg);
+      }
+      return msg;
+    }
     this._deferred.resolve(context, options);
   },
 
   // Resets the list of callbacks to be run, allowing the same list
   // to be run multiple times - whenever the `run` method is called.
   reset: function(){
+    // NOTE: modified for jQuery < 1.5.0 compatibility
+    // @author micheall <micheal.leavitt@popagency.com>
+    if(Marionette.compatMode) {
+      var msg = "$.Deffered not supported by jQuery " + $().jquery + ". Callbacks will not be reset.";
+      if(!_.isUndefined(window.console)) {
+        window.console.warn(msg);
+      }
+      return msg;
+    }
     var that = this;
     var callbacks = this._callbacks;
     this._deferred = $.Deferred();
